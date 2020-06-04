490a16b037a65968c7ef94909e188015ed0ccc2a
jerrykcode <jerrykang026@gmail.com>
2020-06-04 16:39:41 +0800

    store cookies

--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -1,15 +1,36 @@
 package life.majiang.community.controller;
 
+import life.majiang.community.mapper.UserMapper;
+import life.majiang.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        String token = null;
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                token = cookie.getValue();
+                break;
+            }
+        }
+        if (token != null) {
+            User user = userMapper.findByToken(token);
+            if (user != null) {
+                request.getSession().setAttribute("user", user);
+            }
+        }
         return "index";
     }
 }
